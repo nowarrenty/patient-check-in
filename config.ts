fd640d2d168e3814ad8af70297b142524bb10ce5
Warren Harding <whrdng@gmail.com>
2022-06-17 11:55:55 -0500

    feat: add landing page
    Also add empty page that will render form

---
+++ config.ts
@@ -0,0 +1,3 @@
+export const config = {
+  hostpitalName: "The Morant Bay Hospital",
+};
---
+++ pages/form.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Form: NextPage =() => {
+return <div>Form</div>
+}
+
+export default Form
