c5c864571ea7244054d818a13c02f26dcd49e3f5
Warren Harding <whrdng@gmail.com>
2022-06-18 01:28:47 -0500

    feat: add person-details-form shell

---
+++ components/forms/personal-details-form.tsx
@@ -0,0 +1,7 @@
+import { NextComponentType } from "next";
+
+const PersonalDetailsForm: NextComponentType = () => {
+  return <div>Personal Details Form</div>;
+};
+
+export { PersonalDetailsForm };
